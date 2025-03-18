a5a491173813014937670231eb6e03972dcd4d92
harsha5401 <98007205+harsha5401@users.noreply.github.com>
2025-03-18 18:53:19 +0530

    Update test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -1,18 +1,15 @@
-# !/bin/bash
-docker run -d -p 5000:5000 --name flask flask
+#!/bin/bash
+docker run -d -p 5000:5000 --name flask_app flask_app_image
 sleep 5
-num1 =5
-num2 = 10
-expected_sum = $((num1 +num2))
-response = $ (curl ""http://localhost:5000/add?num1=num1&num2=num2")
-if [["$response" == "$expected_sum "]]; then 
-echo "test passed: $num1 + $num2 = $response(expected $expected_sum)"
-Docker stop flask
-docker rm flask
-exit 0
-else 
-echo "Test failed : Expected $expected_sum ,but got $response"
-Docker stop flask
-docker rm flask
-exit 1
+response=$(curl -s "http://localhost:5000/add?num1=5&num2=3")
+if [[ "$response" == "8" ]]; then
+    echo "Test passed!"
+    docker stop flask_app
+    docker rm flask_app
+    exit 0
+else
+    echo "Test failed: Expected 8 but got $response"
+    docker stop flask_app
+    docker rm flask_app
+    exit 1
 fi
